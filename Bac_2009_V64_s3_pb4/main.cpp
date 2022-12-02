1a73c14a46c8433d2ecc7fec4f718581636a37a9
Petean Anamaria <malinapetean@gmail.com>
2022-12-02 21:33:00 +0200

    Update V64_s3_pb4

--- Bac_2009_V64_s3_pb4/main.cpp
+++ Bac_2009_V64_s3_pb4/main.cpp
@@ -7,10 +7,35 @@ ifstream fin("date.in");
 
 int main()
 {
-    int n;
+    int n,v[1000],ct=0,lgmax=0,aux[1000];
     while(fin>>n)
     {
+        v[ct++]=n;
+    }
+    for(int i=0;i<ct-1;i++)
+    {
+        int k=0,secventa[1000]{};
+        while(v[i]>v[i+1] && i<ct)
+        {
+            secventa[k++]=v[i];
+            i++;
+        }
+        if(i<ct)
+            secventa[k++]=v[i];
+        if(lgmax<k)
+        {
+            lgmax=k;
 
+            for(int i=0;i<lgmax;i++)
+            {
+                aux[i]=secventa[i];
+            }
+        }
+    }
+    cout<<lgmax<<endl;
+    for(int i=0;i<lgmax;i++)
+    {
+        cout<<aux[i]<<" ";
     }
     return 0;
 }
